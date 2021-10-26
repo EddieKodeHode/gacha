abf0f03b433ce9733422a48d04ee015d7ca6f10f
EddieKodeHode <eddie.kodehode@gmail.com>
2021-10-26 09:49:21 +0200

    Made more dialogue

--- index.js
+++ index.js
@@ -50,14 +50,20 @@ function rollItemCommon() {
         case 1:
             result.innerHTML = '<div class="win-item" id="common"><h3>1 QP</h3><p>You won 1 Quest Point.</p><p>(common)</p></div>'
             pastResult.innerHTML += '<p class="common">1 QP</p>'
+            shopText.innerHTML = $banterCommon1
+            shopkeep.style.backgroundImage = $shopkeep2
             break;
         case 2:
             result.innerHTML = '<div class="win-item" id="common"><h3>2 QP</h3><p>You won 2 Quest Points.</p><p>(common)</p></div>'
             pastResult.innerHTML += '<p class="common">2 QP</p>'
+            shopText.innerHTML = $banterCommon1
+            shopkeep.style.backgroundImage = $shopkeep2
             break;
         case 3:
             result.innerHTML = '<div class="win-item" id="common"><h3>3 QP</h3><p>You won 3 Quest Points.</p><p>(common)</p></div>'
             pastResult.innerHTML += '<p class="common">3 QP</p>'
+            shopText.innerHTML = $banterCommon2
+            shopkeep.style.backgroundImage = $shopkeep2
             break;
         default:
             console.log('error')
@@ -73,14 +79,20 @@ function rollItemUncommon() {
         case 2:
             result.innerHTML = '<div class="win-item" id="uncommon"><h3>5 QP</h3><p>You won 5 Quest Points</p><p>(uncommon)</p></div>'
             pastResult.innerHTML += '<p class="uncommon">5 QP</p>'
+            shopText.innerHTML = $banterUncommon1
+            shopkeep.style.backgroundImage = $shopkeep3
             break;
         case 3:
             result.innerHTML = '<div class="win-item" id="uncommon"><h3>Common MYO</h3><p>You won a common MYO</p><p>(uncommon)</p></div>'
             pastResult.innerHTML += '<p class="uncommon">MYO</p>'
+            shopText.innerHTML = $banterUncommon2
+            shopkeep.style.backgroundImage = $shopkeep1
             break;
         case 4:
             result.innerHTML = '<div class="win-item" id="uncommon"><h3>Element change scroll</h3><p>You won an element changing scroll</p><p>(uncommon)</p></div>'
             pastResult.innerHTML += '<p class="uncommon">Scroll</p>'
+            shopText.innerHTML = $banterUncommon2
+            shopkeep.style.backgroundImage = $shopkeep1
             break;
         default:
             console.log('error')
@@ -109,6 +121,8 @@ function rollItemRare() {
             console.log('error')
             break;
     }
+    shopText.innerHTML = $banterRare1
+    shopkeep.style.backgroundImage = $shopkeep1
 }
 
 // LEGENDARY POOL
@@ -130,15 +144,17 @@ function rollItemLegendary() {
             break;
     }
     alert("You won a legendary item!")
+    shopText.innerHTML = $banterLegendary1
+    shopkeep.style.backgroundImage = $shopkeep4
 }
 
 // Shopkeeper images and paths
 const shopkeep = document.getElementById("image-goes-here")
 
-const $shopkeep1 = "url(images/shopkeep1.png)"
-const $shopkeep2 = "url(images/shopkeep2.png)"
-const $shopkeep3 = "url(images/shopkeep3.png)"
-const $shopkeep4 = "url(images/shopkeep4.png)"
+const $shopkeep1 = "url(images/shopkeep1.png)" //happy
+const $shopkeep2 = "url(images/shopkeep2.png)" //annoye
+const $shopkeep3 = "url(images/shopkeep3.png)" //neutral
+const $shopkeep4 = "url(images/shopkeep4.png)" //surprised
 
 // Shopkeeper dialogue and paths
 const shopText = document.getElementById("text-goes-here")
@@ -155,6 +171,7 @@ function banter(){
     switch (dialogue) {
         case 1:
             shopText.innerHTML = $banter1
+            shopkeep.style.backgroundImage = $shopkeep1
             break;
         case 2:
             shopText.innerHTML = $banter2
@@ -168,9 +185,21 @@ function banter(){
         default:
             console.log("Error")
             break;
-    }
-    
+    }    
 }
+
+// Rarity roll dialogue
+const $banterCommon1 = "<p>Not bad, but you could do better.</p>"
+const $banterCommon2 = "<p>Oh good, I was hoping to part with this soon.</p>"
+
+const $banterUncommon1 = "<p>Oh, that's where I left that. Guess you can keep it.</p>"
+const $banterUncommon2 = "<p>Not a bad find.</p>"
+
+const $banterRare1 = "<p>I didn't even realize I had one of those. Congratulations, friend.</p>"
+
+const $banterLegendary1 = "<p>What? But that's... A <span class='legendary-col'>legendary</span> item! Well, you won it fair and square. It's all yours to keep.</p>"
+
+
 
 // result.innerHTML = '<div class="win-item" id="common"><h3>ITEMHERE</h3><p>You won a ITEM.</p><p>(common)</p></div>'
 
